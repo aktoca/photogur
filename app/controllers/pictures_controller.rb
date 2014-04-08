84667aaf3bdd1df26e6c3cdc44c1b813b4433480
aktoca <axtoca14@gmail.com>
2014-04-08 13:05:54 -0400

    Making broken links of pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,21 @@
-class PictkresController < ApplicationController
+class PicturesController < ApplicationController
+  def index
+    @pictures = [
+      {
+        :title => "The old church on the coast of the white sea",
+        :artist => "Sergey Ershov",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jgp"
+      },
+      {
+        :title => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jgp"
+      },
+      {
+        :title => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jgp"
+      }
+    ]
+  end
 end
