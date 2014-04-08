240fe113809df315fa462eb74531d1738ec23b75
aktoca <axtoca14@gmail.com>
2014-04-08 13:19:55 -0400

    Refoctoring step 4 to use dynamic routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -18,18 +18,24 @@ class PicturesController < ApplicationController
       }
     ]
   end
-  def picture0
-    @picture = {
+  def show
+    @pictures = [
+      {
         :title => "The old church on the coast of the white sea",
         :artist => "Sergey Ershov",
         :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jgp"
-      }
-    end
-  def picture1
-      @picture = {
+      },
+      {
         :title => "Sea Power",
         :artist => "Stephen Scullion",
         :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jgp"
+      },
+      {
+        :title => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jgp"
       }
+    ]
+    @picture = @pictures[params[:id].to_i]
   end
 end
