937558a9d4d937f512f61e953b5c9a96607c447e
aktoca <axtoca14@gmail.com>
2014-04-08 13:52:32 -0400

    Moving @pictures from pic_contr into the db

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,43 +1,10 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = [
-      {
-        :title => "The old church on the coast of the white sea",
-        :artist => "Sergey Ershov",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jgp"
-      },
-      {
-        :title => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jgp"
-      },
-      {
-        :title => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jgp"
-      }
-    ]
+    @pictures = Picture.all
   end
 
   def show
-    @pictures = [
-      {
-        :title => "The old church on the coast of the white sea",
-        :artist => "Sergey Ershov",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/house.jgp"
-      },
-      {
-        :title => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jgp"
-      },
-      {
-        :title => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jgp"
-      }
-    ]
-    @picture = @pictures[params[:id].to_i]
+    @picture = Pictures.find[params[:id].to_i]
   end
 
   def new
